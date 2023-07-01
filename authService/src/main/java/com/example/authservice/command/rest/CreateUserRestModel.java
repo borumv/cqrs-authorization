b00d4e9c4002dccab1e019fb655fac548d9124af
Falltroy <fallTroy@protonmail.com>
2023-07-01 11:43:04 +0300

    added @Email and @Builder annotations in CreateUserRestModel closes #4

--- authService/src/main/java/com/example/authservice/command/rest/CreateUserRestModel.java
+++ authService/src/main/java/com/example/authservice/command/rest/CreateUserRestModel.java
@@ -1,14 +1,18 @@
-package com.example.authservice.command;
+package com.example.authservice.command.rest;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class CreateUserRestModel {
     private String userId;
     @NotBlank(message = "NickName is a required field")
     private String nickName;
     @NotBlank(message = "email is a required field")
+    @Email(message="Please provide a valid email address")
     private String email;
     private String firstName;
     private String lastName;
